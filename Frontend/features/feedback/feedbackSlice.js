41215035984605e1517e43b35e348fd077578efb
Tarminator-source <ilesanmieniola690@gmail.com>
2026-06-19 19:08:47 +0000

    fix: remove corrupted comment from feedbackSlice.js - build now passes

--- Frontend/features/feedback/feedbackSlice.js
+++ Frontend/features/feedback/feedbackSlice.js
@@ -1,6 +1,5 @@
-import { createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-// Initial state
 const initialState = {
     feedbacks: '',
 }
@@ -9,13 +8,11 @@ export const feedbackSlice = createSlice({
     name: 'feedback',
     initialState,
     reducers: {
-        feedbackState:(state,action) => {
+        feedbackState: (state, action) => {
             state.feedbacks = action.payload;
         }
     }
 })
 
-
-export const {feedbackState} = feedbackSlice.actions;
-
+export const { feedbackState } = feedbackSlice.actions;
 export default feedbackSlice.reducer;
--- Frontend/src/features/feedback/feedbackSlice.js
+++ Frontend/src/features/feedback/feedbackSlice.js
@@ -1,6 +1,5 @@
-import { createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-// Initial state
 const initialState = {
     feedbacks: '',
 }
@@ -9,13 +8,11 @@ export const feedbackSlice = createSlice({
     name: 'feedback',
     initialState,
     reducers: {
-        feedbackState:(state,action) => {
+        feedbackState: (state, action) => {
             state.feedbacks = action.payload;
         }
     }
 })
 
-
-export const {feedbackState} = feedbackSlice.actions;
-
-export default feedbackSlice.reducer;# Build trigger Fri Jun 19 11:07:52 UTC 2026
+export const { feedbackState } = feedbackSlice.actions;
+export default feedbackSlice.reducer;
